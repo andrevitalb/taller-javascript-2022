7aa13c1663a42c5951ee58e1aeb94bc18019fd76
André Vital <andre@andrevital.com>
2022-10-04 12:41:53 -0500

    Added variables js file

---
+++ dia-1/js/variables.js
@@ -0,0 +1,42 @@
+let variable0
+
+const numero1 = 10.1
+const variable2 = "Texto 1"
+const variable3 = true
+const variable4 = null
+const variable5 = undefined
+const variable6 = [1, "Valor de texto", true, undefined, [1, 2, 3, 4]]
+
+// console.log(variable6[1])
+
+variable6[1] = "Nuevo valor de texto"
+
+// console.log(variable6[1])
+
+const variable7 = { nombre: "Juan", edad: 20 }
+
+console.log(variable7.nombre)
+
+variable7.nombre = "Pedro"
+
+console.log(variable7.nombre)
+console.log(variable7)
+
+// console.log(numero1)
+// numero1 = 30
+// console.log(numero1)
+
+// console.log(numero2)
+// numero2 = 30
+// console.log(numero2)
+
+// console.log("Hola mundo desde consola")
+
+// alert("Hola mundo")
+// Lanza una alerta con el mensaje Hola mundo
+
+/*
+    Comentario de varias lineas
+    Linea 1
+    Linea 2
+*/
